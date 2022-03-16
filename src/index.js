5b5dd8958d8ff6a92d170f297b16404180a52c87
Yosua Nicolaus <cryodawn257@gmail.com>
2022-03-16 08:01:40 +0700

    add Todo factory function, getInfo, setInfo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import "sanitize.css";
 import "./style.css";
+
+function Todo(title, description = "-", dueDate = "-", priority = "-") {
+  const getInfo = () => {
+    return { title, description, dueDate, priority };
+  };
+  const setInfo = (newTitle, newDescription, newDueDate, newPriority) => {
+    title = newTitle;
+    description = newDescription;
+    dueDate = newDueDate;
+    priority = newPriority;
+  };
+  const edit = () => {
+    return `editing ${title}`;
+  };
+  const checkStatus = () => {
+    return `checking ${title}`;
+  };
+  return {
+    getInfo,
+    setInfo,
+    edit,
+    checkStatus,
+  };
+}
+
+let x = new Todo("walsk dog");
+console.log(x.getInfo());
+console.log(x.checkStatus());
+
+x.setInfo("go to mars", "just for fun lollz");
+console.log(x.getInfo());
+console.log(x.checkStatus());
